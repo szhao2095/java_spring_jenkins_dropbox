e34b172bb2e9a2ca52674094eb1a8785c22406d4
szhao2095 <szhao2095@gmail.com>
2024-06-10 16:01:06 -0400

    Fixed some warning messages

--- src/main/java/com/example/UserAuth/user/UserController.java
+++ src/main/java/com/example/UserAuth/user/UserController.java
@@ -2,7 +2,6 @@ package com.example.UserAuth.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
